ae82ac157fafd7a18371a5eeb226e2d540e7e605
duza <pavel.mikhadziuk@gmail.com>
2023-12-11 02:07:24 +0100

    #1 Add config with solidjs & tailwindcss
    Should check

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { createSignal } from 'solid-js'
 import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,14 +8,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
         <a href="https://solidjs.com" target="_blank">
           <img src={solidLogo} class="logo solid" alt="Solid logo" />
         </a>
       </div>
-      <h1>Vite + Solid</h1>
+      <h1>Hello at Pavel's site</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count()}
@@ -26,7 +22,7 @@ function App() {
         </p>
       </div>
       <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
+        Click on the Solid logo to learn more
       </p>
     </>
   )
